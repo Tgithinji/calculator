9cd014dd2db3d00341bbf24c5513f7ae22253809
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-02-10 01:47:11 +0300

    Add functionality to all other buttons

--- calc.js
+++ calc.js
@@ -1,3 +1,19 @@
+// global variables
+const numberBtn = document.querySelectorAll('.button');
+const operatorBtn = document.querySelectorAll('.operator');
+const equalsBtn = document.querySelector('.equals');
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const percentageBtn = document.querySelector('.percent');
+const dotBtn = document.querySelector('.dot');
+const negativeBtn = document.querySelector('.negative');
+let displayValue = '';
+let operator = '';
+let numbers = [];
+let lastBtn = false;
+let equals = false;
+
+// function to do the basic math
 function add(numArray) {
     return numArray.reduce((total, currentNum) => {
         return total + currentNum;
@@ -32,26 +48,20 @@ function operate(operator, numArray) {
     else if (operator === '/') return divide(numArray);
 }
 
-const numberBtn = document.querySelectorAll('.button');
-const operatorBtn = document.querySelectorAll('.operator');
-const display = document.querySelector('.display');
-let displayValue = '';
-let operator = '';
-let numbers = [];
-let lastBtn = false;
+// event listener to add functionality to number buttons
 numberBtn.forEach(button => {
         button.addEventListener('click', () => {
-            if (lastBtn) display.textContent = '0'
-            if (display.textContent === '0') display.textContent = ''; 
+            if (display.textContent === '0' || lastBtn) display.textContent = ''; 
             display.textContent += button.textContent;
             displayValue = Number(display.textContent);
             lastBtn = false;
         });
 })
 
-
+// event listener to add functionality to the operator buttons
 operatorBtn.forEach(button => {
     button.addEventListener('click', () => {
+        if (equals) numbers = [];
         numbers.push(displayValue);
         if (!operator) operator = button.textContent;
         let result = operate(operator, numbers);
@@ -59,5 +69,57 @@ operatorBtn.forEach(button => {
         numbers = [result];
         operator = button.textContent;
         lastBtn = true;
+        equals = false;
     })
 })   
+
+// event listener to add functionality to equals button
+equalsBtn.addEventListener('click', () => {
+    numbers.push(displayValue);
+    if (!operator) operator = '+';
+    let result = operate(operator, numbers);
+    display.textContent = result;
+    numbers = [result];
+    operator = '';
+    lastBtn = true;
+    equals = true;
+    displayValue = Number(display.textContent);
+})
+
+// add functionality to clear button
+clearBtn.addEventListener('click', () => {
+    operator = '';
+    numbers = [];
+    display.textContent = 0;
+})
+
+// function to add a decimal point to a number
+dotBtn.addEventListener('click', () => {
+    if (display.textContent.indexOf('.') > -1) return
+    display.textContent += dotBtn.textContent; 
+})
+
+// function to turn a number to percentage
+percentageBtn.addEventListener('click', () => {
+    let num = display.textContent;
+    display.textContent = num / 100;
+    displayValue = Number(display.textContent);
+});
+
+// function to make a negative value
+negativeBtn.addEventListener('click', () => {
+    const num = display.textContent;
+    if (num === '0' || lastBtn) {
+        display.textContent = '-';
+        lastBtn = false;
+        return
+    }
+    if (!num.startsWith('-')) {
+        display.textContent = '-' + num;
+        displayValue = Number(display.textContent);
+    }
+    else {
+        display.textContent = num.replace('-', '');
+        displayValue = Number(display.textContent);
+    }
+})
